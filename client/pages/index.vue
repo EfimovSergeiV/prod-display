a398db7a2030ddf7aa6c4047b04889916e7dc654
anon <efimovsergeiv@gmail.com>
2024-11-05 16:31:11 +0300

    add:

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -6,7 +6,7 @@
 
 
 <template>
-  <div class="flex flex-col justify-between h-screen">
+  <div class="flex flex-col justify-between">
     
 
     <div class="container mx-auto px-4">
@@ -26,15 +26,17 @@
     </div>
 
 
-    <div class="grid grid-cols-3">
-      <div class="w-full bg-red-500 flex items-center justify-center py-4">
-        <p class="">BUTTON 1</p>
-      </div>
-      <div class="w-full bg-blue-500 flex items-center justify-center py-4">
-        <p class="">BUTTON 2</p>
-      </div>
-      <div class="w-full bg-green-500 flex items-center justify-center py-4">
-        <p class="">BUTTON 3</p>
+    <div class=" fixed bottom-0 w-full">
+      <div class="grid grid-cols-3">
+        <div class="w-full bg-red-500 flex items-center justify-center py-4">
+          <p class="">BUTTON 1</p>
+        </div>
+        <div class="w-full bg-blue-500 flex items-center justify-center py-4">
+          <p class="">BUTTON 2</p>
+        </div>
+        <div class="w-full bg-green-500 flex items-center justify-center py-4">
+          <p class="">BUTTON 3</p>
+        </div>
       </div>
     </div>
 
