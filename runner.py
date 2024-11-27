f0419006c82132242267f77d4fed50a308d5f0e9
anon <efimovsergeiv@gmail.com>
2024-11-27 11:35:50 +0300

    add:

--- runner.py
+++ runner.py
@@ -1,23 +1,33 @@
 """ Запуск приложений """
 import os, requests
 import subprocess
+from time import sleep
 
 # Установка переменной окружения DISPLAY
 os.environ["DISPLAY"] = ":0"
 
-server_status = requests.get("http://mon1.local:8080/").status_code
-client_status = requests.get("http://mon1.local/").status_code
 
-print('СТАТУС СЕРВЕРОВ: ', server_status, client_status)
-
-
-if server_status == 200 and client_status == 200:
+while True:
+    server_status = requests.get("http://mon1.local:8080/").status_code
+    client_status = requests.get("http://mon1.local/").status_code
     
-    subprocess.run(["firefox-esr", "--kiosk", "http://mon1.local"])
+    if server_status == 200 and client_status == 200:
+        print('ЗАПУСК - СТАТУС СЕРВЕРОВ: ', server_status, client_status)
+        subprocess.run(["firefox-esr", "--kiosk", "http://mon1.local"])
 
-
+    print('ОЖИДАНИЕ - СТАТУС СЕРВЕРОВ: ', server_status, client_status)
+    sleep(10)
 
 
 
 
 # os.system("firefox-esr --kiosk http://mon1.local:8000")
+
+"""
+[Desktop Entry]
+Type=Application
+Exec=firefox --kiosk
+Hidden=false
+X-LXDE-Autostart-enabled=true
+Name=Firefox Fullscreen
+"""
